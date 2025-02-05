9a7a8d5ce209f66a56579c3666778637e02dea1d
yoshiki-tachi-spiderplus <yoshiki.tachi@spiderplus.co.jp>
2025-02-05 10:27:44 +0900

    fix

---
+++ Sample/NameWarning.swift
@@ -0,0 +1,32 @@
+//
+//  NameWarning.swift
+//  Sample
+//
+//  Created by sp_user on 2025/02/05.
+//
+
+import Foundation
+
+final class test_NameWorning {
+    func testMethod2(a: Int, b: Int, c: Int, d: Int, e: Int, f: Int, g: Int) {
+        if a == 0 {
+            if b == 1 {
+                if c == 2 {
+                    if d == 3 {
+                        print("aaa")
+                    } else {
+                        print("vvv")
+                    }
+                    
+                    if e == 0 {
+                        if f == 0 {
+                            if g == 0 {
+                                print("vvv")
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
